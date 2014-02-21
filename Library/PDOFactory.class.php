3936370f3429fa8a07ce2f3265f70eb8b9f1e33b
Adam <adam.menebhi@gmail.com>
2014-02-21 12:11:37 +0100

    fuck

--- Library/PDOFactory.class.php
+++ Library/PDOFactory.class.php
@@ -5,7 +5,7 @@ class PDOFactory
 {
 	public static function getMysqlConnexion()
 	{
-		$db = new \PDO('mysql:host=localhost;dbname=spa', 'root', 'toor');
+		$db = new \PDO('mysql:host=localhost;dbname=spa', 'root', 'root');
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
      
 		return $db;
